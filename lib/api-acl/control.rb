c8f3ccfadbff67b09a0a13b122c01e7dc16ba288
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-05 14:08:39 -0300

    Add basic methods to set and get arbitrary variables in controller

---
+++ lib/api-acl/control.rb
@@ -0,0 +1,37 @@
+module ACL
+  module Control
+    
+    def self.put(variable)
+      controller.put(variable)
+      return self
+    end
+
+    def self.get(value)
+      controller.variables[value]
+    end
+
+    def self.method_missing(*args)
+      if args.length == 2 then #set value
+        put ({args[0].to_s.chomp('=').to_sym => args[1]})
+      else #return value
+        return get(args.first)
+      end
+    end
+
+    def self.controller
+      @access_controller ||=AccessController.new
+    end
+
+    class AccessController
+      attr_reader :variables
+
+      def initialize
+        @variables = Hash.new 
+      end
+
+      def put(variable)
+        @variables.merge!(variable)
+      end
+    end
+  end
+end
