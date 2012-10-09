a1c36e009e2b35bc5e193aa7d05067ebf2670c7d
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-09 13:59:08 -0300

    Add example

---
+++ examples/usage_example.rb
@@ -0,0 +1,25 @@
+require 'api-acl'
+require_relative 'rules_example'
+
+module ACL
+  module Validators
+
+    def self.global
+      return true
+    end
+
+    def self.arbitrary_validator
+      return true
+    end
+  end
+end
+
+ACL.configure do |config|
+  config.force_access_control = true
+  config.global_validators = ['global']
+end
+
+MyRule.new #Use defined validators
+
+access_level = ACL::Control.put({:consumer => ''}).put({:user => ''}).check_level('test','POST')
+p access_level
