16096597a0224a4e6d97c60a1f6008cb35bf3286
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-10 19:24:48 -0300

    Remove rules.example, integrate into usage_example

--- examples/rules.example.rb
+++
@@ -1,8 +0,0 @@
-require 'api-acl'
-
-ACL::when_route 'test', :with_method => 'POST', :get_access_level => 1, :if_pass => ['User.test1']
-ACL::when_route 'test', :with_method => 'POST', :get_access_level => 2, :if_pass => ['User.test2', 'asd']
-ACL::when_route 'test', :with_method => 'GET', :get_access_level => 1, :if_pass => ['Test.1']
-ACL::when_route 'test', :with_method => 'GET', :get_access_level => 2, :if_pass => []
-
-p ACL::acl_routes_collection
--- examples/usage_example.rb
+++ examples/usage_example.rb
@@ -1,26 +1,27 @@
 require 'api-acl'
-require_relative 'rules_example'
-
-module ACL
-  module Validators
-
-    def self.global
-      return true
-    end
-
-    def self.arbitrary_validator
-      return true
-    end
-  end
+#Configuration:
+ACL::configure do |config|
+  config.api_acl_mode = ACL::Configuration::STRATEGY_DESCENDING
+  config.force_access_control = true #Deny access if no validators are given for a route/method combination and no global validators exist
 end
 
-ACL.configure do |config|
-  config.force_access_control = true
-  config.global_validators = ['global']
-  config.api_acl_mode = ACL::Configuration::STRATEGY_DESCENDING #this is the default value
-end
+#Define variables
+ACL::some_arbitrary_value = true
 
-MyRule.new #Use defined validators
+#Define some validators:
+#Global
+ACL.add_global_validator("global_validator",lambda{return true}) #Global validators are executed for every route
+#
+ACL::add_validator("validator",lambda{return some_arbitrary_value})
+ACL::add_validator("another_validator",lambda{return false})
 
-access_level = ACL::Control.put({:consumer => ''}).put({:user => ''}).check_level('test','POST')
-p access_level
+#Define routes
+ACL::when_route 'test', :with_method => 'POST', :get_access_level => 1, :if_pass => ['validator']
+ACL::when_route 'test', :with_method => 'POST', :get_access_level => 2, :if_pass => ['validator', 'another_validator']
+ACL::when_route 'test', :with_method => 'GET', :get_access_level => 1, :if_pass => ['another_validator']
+ACL::when_route 'test', :with_method => 'GET', :get_access_level => 2, :if_pass => []
+
+access_level = ACL::check_level('test','POST')
+puts access_level
+access_level = ACL::check_level('test','GET')
+puts access_level
