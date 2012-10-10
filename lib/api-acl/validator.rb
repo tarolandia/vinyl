25f4d608df87b0715ac21c5d301bd60ef7e7007f
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-10 19:27:20 -0300

    Add prefix to validator method to avoid collission with variable names - Modify definition mechanism for global validators

--- lib/api-acl/validator.rb
+++ lib/api-acl/validator.rb
@@ -1,21 +1,37 @@
 module ACL
 
+  @@prefix = "validator_"#use prefixs to avoid collisions betweeen variables and methods names
+
   def self.add_validator(name,block)
-    Validators.module_eval{define_singleton_method(name,block)}
+    Validators.module_eval{define_singleton_method(@@prefix + name,block)}
+  end
+
+  def self.add_global_validator(name,block)
+    Validators.module_eval{define_singleton_method(@@prefix + name,block)}
+    global_validators << name
   end
 
   def self.execute(name)
-    Validators.send(name)
+    Validators.send(@@prefix + name)
+  end
+
+  def self.prefix
+    @@prefix
+  end
+
+  def self.global_validators
+    @@global_validators ||= Array.new
   end
 
   module Validators
+
     def self.run_validators(validators)
-      validators.concat(ACL::config.global_validators)
+      validators.concat(ACL.global_validators)
       if validators.nil? || validators.empty? then
         return true
       end
       validators.each do |method_name|
-        pass = ACL::Validators.send(method_name)
+        pass = ACL::Validators.send(ACL.prefix + method_name)
         if(pass == false) then
           return false
         end
