77d57712e1b19f4fb604d5bb8d030316738cf4b9
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-10 16:35:09 -0300

    Update rules examle

--- examples/rules.example.rb
+++ examples/rules.example.rb
@@ -1,12 +1,8 @@
 require 'api-acl'
 
-class MyRule 
-  include ACL::Rules::Collection
-  when_route 'test', :with_method => 'POST', :get_access_level => 1, :if_pass => ['User.test1']
-  when_route 'test', :with_method => 'POST', :get_access_level => 2, :if_pass => ['User.test2', 'asd']
-  when_route 'test', :with_method => 'GET', :get_access_level => 1, :if_pass => ['Test.1']
-  when_route 'test', :with_method => 'GET', :get_access_level => 2, :if_pass => []
-end
+ACL::when_route 'test', :with_method => 'POST', :get_access_level => 1, :if_pass => ['User.test1']
+ACL::when_route 'test', :with_method => 'POST', :get_access_level => 2, :if_pass => ['User.test2', 'asd']
+ACL::when_route 'test', :with_method => 'GET', :get_access_level => 1, :if_pass => ['Test.1']
+ACL::when_route 'test', :with_method => 'GET', :get_access_level => 2, :if_pass => []
 
-
-puts MyRule.acl_routes_collection
+p ACL::acl_routes_collection
