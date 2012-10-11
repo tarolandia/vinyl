5b1f95d7100293c522d74e63cc8a4bf7cdf9d400
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-11 11:25:58 -0300

    Add specs for variables

--- tests/gem_spec.rb
+++ tests/gem_spec.rb
@@ -1,3 +1,4 @@
 require_relative 'spec_helper'
 require_relative 'rules_spec'
+require_relative 'variables_spec'
 require_relative 'validators_spec'
---
+++ tests/variables_spec.rb
@@ -0,0 +1,11 @@
+describe ACL::UserVariables do
+
+  before do
+  end
+  
+  it "should add some variables to be used by validators" do
+    ACL::some_arbitrary_value = true
+
+  end
+
+end
