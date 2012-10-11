0d265a0b91f8aa5552ce3f83c889efd6f3d56f36
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-11 12:21:29 -0300

    Convert instance variables into global variables to be used by other tests

--- tests/variables_spec.rb
+++ tests/variables_spec.rb
@@ -1,11 +1,20 @@
 describe ACL::UserVariables do
 
   before do
+    $string_variable = "Test string"
+    $hash_variable = Hash.new
   end
-  
+
   it "should add some variables to be used by validators" do
-    ACL::some_arbitrary_value = true
-
+    ACL.some_arbitrary_value = true
+    ACL.string_value = $string_variable
+    ACL.hash_value = $hash_variable
+    ACL.controller.variables.length.should == 3
   end
 
+  it "should be able to access to previous defined variables"  do
+    ACL.some_arbitrary_value.should be_true
+    ACL.string_value.should eq $string_variable
+    ACL.hash_value.should == $hash_variable
+  end
 end
