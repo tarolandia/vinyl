50c245356fc7974a6f188b504b9375a8a532502f
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-11 13:32:35 -0300

    Add more rules tests

--- tests/rules_spec.rb
+++ tests/rules_spec.rb
@@ -10,14 +10,23 @@ describe ACL::Rule do
 
   it "should add multiple access level to route" do
     ACL::when_route 'test', :with_method => 'POST', :get_access_level => 1, 
-      :if_pass => ['validator']
+      :if_pass => ['validator','bad_guy']
     ACL::when_route 'test', :with_method => 'POST', :get_access_level => 2, 
       :if_pass => ['validator']
+    ACL::when_route 'test', :with_method => 'POST', :get_access_level => 3, 
+      :if_pass => []
     
     ACL.acl_routes_collection['test'].should_not be_empty  
     ACL.acl_routes_collection.length.should == 1
     ACL.acl_routes_collection['test'].should include("POST")
-    ACL.acl_routes_collection['test']['POST'].length.should == 2
+    ACL.acl_routes_collection['test']['POST'].length.should == 3
   end
 
+  it "should add a route with a non-existent validator" do
+    ACL::when_route 'test', :with_method => 'GET', :get_access_level => 1, 
+      :if_pass => ['fake_validator']
+    ACL.acl_routes_collection['test'].should include("GET")
+    ACL.acl_routes_collection['test']['GET'].length.should == 1
+    ACL.execute('fake_validator').should be_false
+  end
 end
