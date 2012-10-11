f513922440c6233350a44b2524124b952741c8c8
Federico Saravia Barrantes <fedesaravia@gmail.com>
2012-10-11 13:33:25 -0300

    Remove global validators declaration to be able to test every alternative

--- tests/validators_spec.rb
+++ tests/validators_spec.rb
@@ -1,16 +1,13 @@
 describe "Validators" do
 
-  it "should add a global validator to ACL" do
-    global_validator_name = "global_validator"
-    ACL.add_global_validator(global_validator_name,lambda{return true})
-    result = ACL.execute(global_validator_name)
-    result.should be_true
-  end
-
   it "should add some validators to ACL" do
     ACL.add_validator('validator', lambda{
       return some_arbitrary_value && hash_value == Hash.new
       })
     ACL.execute('validator').should be_true
+    ACL.add_validator('bad_guy', lambda{
+      return false
+      })
+    ACL.execute('bad_guy').should be_false
   end
 end
