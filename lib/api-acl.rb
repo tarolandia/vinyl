05314e3e3c4e097a4fffb9a4a189090935cd66da
Lautaro Orazi <orazile@gmail.com>
2012-10-23 12:57:10 -0300

    rename files

--- lib/api-acl.rb
+++
@@ -1,4 +0,0 @@
-require 'api-acl/base'
-require 'api-acl/validator'
-require 'api-acl/variables'
-require 'api-acl/rule'
---
+++ lib/vinyl.rb
@@ -0,0 +1,4 @@
+require 'vinyl/base'
+require 'vinyl/validator'
+require 'vinyl/variables'
+require 'vinyl/rule'
