fd7b89803aace9d335e7f061367af997b5d10e64
Lautaro Orazi <orazile@gmail.com>
2012-10-27 15:54:10 -0300

    Group routes under method

--- tests/rules_spec.rb
+++ tests/rules_spec.rb
@@ -16,17 +16,17 @@ describe Vinyl::Rule do
     Vinyl::when_route 'test', :with_method => 'POST', :get_access_level => 3, 
       :if_pass => []
     
-    Vinyl.acl_routes_collection['test'].should_not be_empty  
+    Vinyl.acl_routes_collection['POST'].should_not be_empty  
     Vinyl.acl_routes_collection.length.should == 1
-    Vinyl.acl_routes_collection['test'].should include("POST")
-    Vinyl.acl_routes_collection['test']['POST'].length.should == 3
+    Vinyl.acl_routes_collection['POST']['test'].should be_instance_of(Hash)
+    Vinyl.acl_routes_collection['POST']['test'].length.should == 3
   end
 
   it "should add a route with a non-existent validator" do
     Vinyl::when_route 'test', :with_method => 'GET', :get_access_level => 1, 
       :if_pass => ['fake_validator']
-    Vinyl.acl_routes_collection['test'].should include("GET")
-    Vinyl.acl_routes_collection['test']['GET'].length.should == 1
+    Vinyl.acl_routes_collection['GET']['test'].should be_instance_of(Hash)
+    Vinyl.acl_routes_collection['GET']['test'].length.should == 1
     Vinyl.execute('fake_validator').should be_false
   end
 end
